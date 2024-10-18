94fd7b3df4618e9ac7ef9404119854c55dd58ecf
abeebdon <abeebdon@gmail.com>
2024-10-18 16:11:12 +0100

    init

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils'
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, contentClassName, value, ...props }, ref) => (
+>(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -16,10 +16,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={cn(
-        'h-full w-full flex-1 bg-primary transition-all ',
-        contentClassName
-      )}
+      className="h-full w-full flex-1 bg-primary transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
